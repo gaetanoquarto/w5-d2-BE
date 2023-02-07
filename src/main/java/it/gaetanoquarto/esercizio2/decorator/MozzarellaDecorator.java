1bbbfb72990e213b687b466e4efd44277496e16b
gaetanoquarto <gaetanoqquarto@gmail.com>
2023-02-07 17:37:59 +0100

    commit

---
+++ src/main/java/it/gaetanoquarto/esercizio2/decorator/MozzarellaDecorator.java
@@ -0,0 +1,35 @@
+package it.gaetanoquarto.esercizio2.decorator;
+
+import it.gaetanoquarto.esercizio2.beans.Pizza;
+import it.gaetanoquarto.esercizio2.beans.Topping;
+import lombok.ToString;
+
+@ToString
+public class MozzarellaDecorator extends Topping{
+
+	public MozzarellaDecorator(Pizza pizza) {
+		this.pizza = pizza;
+		this.nomeTopping = "mozzarella";
+		this.calorie = 20;
+		this.prezzo = 0.10;
+
+
+	}
+	
+
+	@Override
+	public String getNome() {
+		return pizza.getNome() + "mozzarella ";
+	}
+	
+	@Override
+	public double getPrezzo() {
+		return pizza.getPrezzo() + prezzo;
+	}
+	
+	@Override
+	public int getCalorie() {
+		return pizza.getCalorie() + calorie;
+	}
+
+}
---
+++ src/test/java/it/gaetanoquarto/esercizio2/Week5Day1BeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gaetanoquarto.esercizio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5Day1BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
